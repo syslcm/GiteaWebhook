62d128da4b55c47b1e703b5e60b5dfa46233d093
tw009077 <james_song@usiglobal.com>
2020-09-15 18:15:51 +0800

    REST/push rest -- 由 header 取 signature , event 值

--- src/main/java/org/james/HookRest.java
+++ src/main/java/org/james/HookRest.java
@@ -12,17 +12,33 @@ public class HookRest {
     @PostMapping(value = "/push")
     public String doThings(@RequestBody String text, @RequestHeader Map<String, String> headers){
         String signature = "no data";
-        if (headers.get("HTTP_X_GITEA_SIGNATURE") != null){
-            signature = headers.get("HTTP_X_GITEA_SIGNATURE");
-        }else if (headers.get("X_GITEA_SIGNATURE") != null){
-            signature = headers.get("X_GITEA_SIGNATURE");
+        String giteaEvent = "nuknown event";
+        if (headers.get("x-gitea-signature") != null){
+            signature = headers.get("x-gitea-signature");
+        }
+        if (headers.get("x-gitea-event") != null){
+            giteaEvent = headers.get("x-gitea-event");
         }
         System.out.println("GITEA_SIGNATURE:" + signature) ;
+        System.out.println("gitea-event:" + giteaEvent) ;
         System.out.println(text);
-        for (String header : headers.keySet()){
-            System.out.println(header);
-        }
         return text;
     }
 
 }
+
+/*
+
+host
+        user-agent
+        content-length
+        content-type
+        x-github-delivery
+        x-github-event
+        x-gitea-delivery
+        x-gitea-event
+        x-gitea-signature
+        x-gogs-delivery
+        x-gogs-event
+        x-gogs-signature
+        accept-encoding*/
