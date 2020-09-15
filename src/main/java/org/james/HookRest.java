44a082e1294839f0738e321495c6a60ffe82202a
tw009077 <james_song@usiglobal.com>
2020-09-15 18:06:12 +0800

    REST/push rest -- 加取 header 值

--- src/main/java/org/james/HookRest.java
+++ src/main/java/org/james/HookRest.java
@@ -2,13 +2,22 @@ package org.james;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 
 @RestController
 @RequestMapping(path = "/hook")
 public class HookRest {
 
     @PostMapping(value = "/push")
-    public String doThings(@RequestBody String text){
+    public String doThings(@RequestBody String text, @RequestHeader Map<String, String> headers){
+        String signature = "no data";
+        if (headers.get("HTTP_X_GITEA_SIGNATURE") != null){
+            signature = headers.get("HTTP_X_GITEA_SIGNATURE");
+        }else if (headers.get("X_GITEA_SIGNATURE") != null){
+            signature = headers.get("X_GITEA_SIGNATURE");
+        }
+        System.out.println("GITEA_SIGNATURE:" + signature) ;
         System.out.println(text);
         return text;
     }
